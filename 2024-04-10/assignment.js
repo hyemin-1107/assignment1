44b90bd8ad5acdcbe76bba22a67cf0f122098293
hyemin-1107 <hyem92.11.07@gmail.com>
2024-04-11 11:08:17 +0900

    assignment

--- 2024-04-10/assignment.js
+++ 2024-04-10/assignment.js
@@ -23,6 +23,14 @@ function solution(dot) {
 }
 
 function solution(dot) {
+    if (dot[0]>0) {
+        return dot[1]>0 ? 1 : 4;
+    } else {
+        return dot[1]>0 ? 2 : 3;
+    }
+}
+
+function solution(dot) {
     const [num,num2] = dot;
     const check = num * num2 > 0;
     return num > 0 ? (check ? 1 : 4) : (check ? 3 : 2);
